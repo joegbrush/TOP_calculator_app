2f31159e907605228b32638863d9834d587941aa
Joe Brush <joegbrush@gmail.com>
2022-06-26 14:07:14 -0500

    add, subtract, multiply, divied functions added

---
+++ app.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+  let sum = a + b;
+  console.log(sum);
+  return sum;
+}
+
+function subtract(a, b) {
+  let diff = a - b;
+  console.log(diff);
+  return diff;
+}
+
+function multiply(a, b) {
+  let product = a * b;
+  console.log(product);
+  return product;
+}
+
+function divide(a, b) {
+  let quotient = a / b;
+  console.log(quotient);
+  return quotient;
+}
+
+add(2, 2);
+subtract(2, 2);
+multiply(2, 2);
+divide(2, 2);
